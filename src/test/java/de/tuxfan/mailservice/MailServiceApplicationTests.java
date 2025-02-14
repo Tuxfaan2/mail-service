8c5d0e2286300a23aaa88dee403d861de4019625
Tuxfan <jonas@familie-wuepping.de>
2025-02-14 08:38:58 +0100

    Initial commit

---
+++ src/test/java/de/tuxfan/mailservice/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tuxfan.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
